31d04b0eff637c9a665ad2887958f04458cb248a
Planxnx <planxthanee@gmail.com>
2026-03-30 21:47:24 +0700

    feat: add zod schema for primitive type
    Co-authored-by: Planxnx <planxnx@users.noreply.github.com>

--- src/types/Utility/primitive.ts
+++ src/types/Utility/primitive.ts
@@ -1,9 +1,27 @@
+import { z } from "zod"
+
 /**
  * Primitive type with not null or undefined
  */
 export type PrimitiveValue = string | number | boolean
 
 /**
+ * Zod schema for PrimitiveValue
+ *
+ * NOTE:
+ * We don't use `z.infer` to reduce bundle size and type inference performance.
+ */
+export const PrimitiveValueSchema = z.union([z.string(), z.number(), z.boolean()])
+
+/**
  * Primitive type
  */
 export type Primitive = PrimitiveValue | null | undefined
+
+/**
+ * Zod schema for Primitive
+ *
+ * NOTE:
+ * We don't use `z.infer` to reduce bundle size and type inference performance.
+ */
+export const PrimitiveSchema = z.union([PrimitiveValueSchema, z.null(), z.undefined()])
